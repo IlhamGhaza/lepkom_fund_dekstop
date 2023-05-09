5f69805f37a45e7a5ff9779d65f591d8a0317b5b
Ghazali <57607688+IlhamGhaza@users.noreply.github.com>
2023-05-09 15:25:49 +0700

    Create Program.vb

---
+++ M4/P2_4/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim sLuas As Single
+        Dim sPanjang As Single = 7.5689782
+        Dim sLebar As Double = 9.568972
+        Dim sLuas2 As Double
+        Dim sPanjang2 As Double = 7.5689782
+        Dim sLebar2 As Double = 9.568972
+
+        sLuas = sPanjang * sLebar
+        sLuas2 = sPanjang2 * sLebar2
+        MsgBox("Hasil dengan type Single adalah " = sLuas.ToString() & vbNewLine & "Hasil dengan type Double adalah " =
+               sLuas2.ToString(), MsgBoxStyle.Information, "Tipe Data")
+
+    End Sub
+End Module
