3be265f1d7bc62e00070b2628f2275b392cdcf34
Ghazali <57607688+IlhamGhaza@users.noreply.github.com>
2023-05-30 09:07:53 +0700

    Create array.java

---
+++ M6/array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main (String [] args){
+        String[] kota = {"Jakarta", "Bandung","Semarang","Medan","Yogya"}; 
+        System.out.println(kota[0]);
+        System.out.println(kota[1]);
+        System.out.println(kota[2]);
+        System.out.println(kota[3]);
+        System.out.println(kota[4]);
+    }
+    
+}
